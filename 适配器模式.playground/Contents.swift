ac5a2627d07cdbade95446c40ba63fce280141a6
fengcaifan <fengcaifan@baidu.com>
2022-06-20 15:35:33 +0800

    适配器模式

--- 适配器模式.playground/Contents.swift
+++ 适配器模式.playground/Contents.swift
@@ -1,3 +1,40 @@
 import UIKit
 
 debugPrint("-------------适配器模式------------")
+
+struct OldTarget {
+    let width: Float
+    let height: Float
+    
+    init(width: Float, height: Float) {
+        self.width = width
+        self.height = height
+    }
+}
+
+// 构建一个协议
+protocol TargetProtocol {
+    var width: Float { get }
+    var height: Float { get }
+}
+
+/// 新target持有旧target
+struct NewTarget: TargetProtocol {
+    private let oldTarget: OldTarget
+    
+    var width: Float {
+        return oldTarget.width
+    }
+    
+    var height: Float {
+        return oldTarget.height
+    }
+    
+    init(_ target: OldTarget) {
+        self.oldTarget = target
+    }
+}
+
+let target = OldTarget(width: 32.0, height: 32.0)
+let newFormat = NewTarget(target)
+debugPrint(newFormat.width, newFormat.height)
